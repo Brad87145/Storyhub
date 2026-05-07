9b8c57d0531415bce96aa241366ce6159e9d75e8
Brad <brad87145@gmail.com>
2026-05-07 19:20:48 +0300

    Update book.js

--- book.js
+++ book.js
@@ -1,1 +1,17 @@
+const params =
+  new URLSearchParams(window.location.search);
 
+const index =
+  params.get("index");
+
+const books =
+  JSON.parse(localStorage.getItem("books")) || [];
+
+const book =
+  books[index];
+
+document.getElementById("book-title").textContent =
+  book.title;
+
+document.getElementById("book-description").textContent =
+  book.description;
