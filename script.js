e4a87da7fb16bcdc3d4f6f9cbe16e3e80d3db190
Brad <brad87145@gmail.com>
2026-05-08 14:05:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ button.addEventListener("click", () => {
   const title =
     document.getElementById("title").value;
 
+  const description =
+    document.getElementById("description").value;
+
   const card =
     document.createElement("div");
 
+  card.classList.add("book-card");
+
   card.innerHTML = `
     <h2>${title}</h2>
+    <p>${description}</p>
   `;
 
   list.appendChild(card);
