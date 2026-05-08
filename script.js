40a8c7f7cf93c073dc301a51348486b9910e6bcb
Brad <brad87145@gmail.com>
2026-05-08 13:54:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("JavaScript is connected!");
+const button =
+  document.getElementById("publish-btn");
+
+const list =
+  document.getElementById("book-list");
+
+button.addEventListener("click", () => {
+
+  const title =
+    document.getElementById("title").value;
+
+  const card =
+    document.createElement("div");
+
+  card.innerHTML = `
+    <h2>${title}</h2>
+  `;
+
+  list.appendChild(card);
+
+});
